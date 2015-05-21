8790b02c21ffc6d01676920e97297eadf36b0b9e
Soar <85624529@qq.com>
2015-05-21 22:26:22 +0800

    change to use image_tag

--- config.rb
+++ config.rb
@@ -57,7 +57,10 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
-
+ 
+(1..8).each do |i|
+  proxy "/pages/detail#{i}.html", "/pages/detail.html"
+end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
